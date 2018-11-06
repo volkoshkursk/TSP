682dcddd9738ab2e7ea9e5fbc30fa90ea1bade36
volkoshkursk <zaz-pagani@inbox.ru>
2018-11-06 10:13:30 +0300

    add bound calculation

--- TSP.py
+++ TSP.py
@@ -1,16 +1,18 @@
 import numpy as np
 
-# from random import random
-
 
 def generate_task(num):
-    return np.random.sample((num, num))
+    out = np.random.sample((num, num))
+    for i in range(num):
+        out[i][i] = np.float('inf')
+    return out
 
 
 class TSP:
     """
     класс задача коммивояжера
     """
+
     def __init__(self, matrix, num_of_towns):
         self.matrix = matrix
         self.num_of_towns = num_of_towns
@@ -25,6 +27,15 @@ class TSP:
         s.append(0)
         return self.f(s)
 
+    def __lower_bound__(self, left):
+        """
+        нижняя гранца
+        :param left: список городов, которые необходимо посетить
+        :return:
+        """
+        left_matrix = np.array([[self.matrix[j][i] for i in left] for j in left])
+        return sum(left_matrix.min(axis=1))
+
     def f(self, s):
         """
         целевая функция
@@ -37,5 +48,13 @@ class TSP:
 
 
 if __name__ == '__main__':
-    generate_task(3)
-
+    #    t = TSP(generate_task(5), 5)
+    a = generate_task(5)
+    matrix = np.array([np.float('inf'), 10, 25, 25, 10,
+                       1, np.float('inf'), 10, 15, 2,
+                       8, 9, np.float('inf'), 20, 10,
+                       14, 10, 24, np.float('inf'), 15,
+                       10, 8, 25, 27, np.float('inf')])
+    t = TSP(matrix.reshape((5, 5)), 5)
+    print(t.__high_bound__([0, 1, 2, 3, 4]))
+    print(t.__lower_bound__([0, 1, 2, 3, 4]))
